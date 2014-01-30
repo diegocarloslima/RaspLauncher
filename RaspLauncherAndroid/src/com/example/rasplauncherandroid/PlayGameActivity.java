d3cbc1611514c082f0a025fc9fc3be3fa0327cfb
Diego Lima <diegocarloslima@hotmail.com>
2014-01-30 19:25:29 -0200

    Adding initial Android files

---
+++ RaspLauncherAndroid/src/com/example/rasplauncherandroid/PlayGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.rasplauncherandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayGameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.play_game);
+	}
+}
---
+++ RaspLauncherAndroid/src/com/example/rasplauncherandroid/StartGameActivity.java
@@ -0,0 +1,28 @@
+package com.example.rasplauncherandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class StartGameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_game);
+        
+        final EditText startGameUsername = (EditText) findViewById(R.id.start_game_username);
+        
+        final View startGameButton = findViewById(R.id.start_game_button);
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				final Intent intent = new Intent(StartGameActivity.this, PlayGameActivity.class);
+				startActivity(intent);
+			}
+		});
+    }
+}
